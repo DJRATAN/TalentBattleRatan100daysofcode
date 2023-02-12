72289d354f7454b272fd8ee3a602a13169609df4
Ratan Prajapati <ratanprajapti1242@gmail.com>
2023-02-12 21:55:15 +0530

    12-02-2023 day 26 challenge

---
+++ RatanPrajapati_day26/RatanPrajapati_day26.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class RatanPrajapati_day26 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int ans = num*(num-1)/2;
+        System.out.println(ans);
+    }
+}
