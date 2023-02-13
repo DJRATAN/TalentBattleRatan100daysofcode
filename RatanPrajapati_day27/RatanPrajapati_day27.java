71d6ae58d936f3e63508a9ba5120fb71f10e3ac7
Ratan Prajapati <ratanprajapti1242@gmail.com>
2023-02-13 23:41:59 +0530

    13-02-2023 day 27 challenge

---
+++ RatanPrajapati_day27/RatanPrajapati_day27.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class RatanPrajapati_day27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        num = num << 1;
+        System.out.println(num);
+    }
+}
