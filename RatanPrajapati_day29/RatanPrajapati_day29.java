072b90db205e5aa611ecba65e8210896fe1e919d
Ratan Prajapati <ratanprajapti1242@gmail.com>
2023-02-15 21:16:51 +0530

    15-02-2023 day 29 challenge

---
+++ RatanPrajapati_day29/RatanPrajapati_day29.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class RatanPrajapati_day29 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first string");
+        String st1 = sc.nextLine();
+        System.out.println("Enter second string");
+        String st2 = sc.nextLine();
+        System.out.println(st1.concat(st2));
+    }
+}
