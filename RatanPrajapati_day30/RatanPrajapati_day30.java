1d3e4ff74c6b19a2d89e3e18d52439d3c58454a3
Ratan Prajapati <ratanprajapti1242@gmail.com>
2023-02-16 21:55:13 +0530

    16-02-2023 day 30 challenge

---
+++ RatanPrajapati_day30/RatanPrajapati_day30.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class RatanPrajapati_day30 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        int count = 0;
+        for (int i = 0; i < st.length(); i++) {
+            if (st.charAt(i) != ' ') {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
