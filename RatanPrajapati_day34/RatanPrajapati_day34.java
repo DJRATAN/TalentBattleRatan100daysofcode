f9121cd90ef0170ac5a1c6d26ee6e6862a3770a3
Ratan Prajapati <ratanprajapti1242@gmail.com>
2023-02-20 23:24:11 +0530

    20-02-2023 day 34 challenge

--- RatanPrajapati_day34/RatanPrajapati_day34.java
+++ RatanPrajapati_day34/RatanPrajapati_day34.java
@@ -4,6 +4,7 @@ public class RatanPrajapati_day34 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String st = sc.nextLine();
+        
         String st2 = "";
         for (int i = 0; i < st.length(); i++) {
             if (st.charAt(i) == '(' || st.charAt(i) == ')') {
