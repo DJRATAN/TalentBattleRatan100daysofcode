0c48a69436381808048bcf257141260efa9deca6
Ratan Prajapati <ratanprajapti1242@gmail.com>
2023-02-20 23:22:25 +0530

    20-02-2023 day 33 challenge

---
+++ RatanPrajapati_day33/RatanPrajapati_day33.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class RatanPrajapati_day33 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        String st2 = "";
+        int len = st.length()-1;
+        boolean is = true;
+        for (int i = 0; i < len / 2; i++) {
+            if (st.charAt(i) != st.charAt(len)) {
+                is = false;
+                break;
+            }
+        }
+        if (is) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
