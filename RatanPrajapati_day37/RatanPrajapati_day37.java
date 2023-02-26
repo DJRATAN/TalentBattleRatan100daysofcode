9046fc68440e58a409b60cec17348ec407f3d76e
Ratan Prajapati <ratanprajapti1242@gmail.com>
2023-02-26 22:39:09 +0530

    23-02-2023 day 37 challenge

---
+++ RatanPrajapati_day37/RatanPrajapati_day37.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class RatanPrajapati_day37 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        char[] ch = st.toCharArray();
+        int[] count = new int[st.length()];
+        for (int i = 0; i < st.length(); i++) {
+            count[i] = 1;
+            for (int j = i + 1; j < st.length(); j++) {
+                if (ch[i] == ch[j]) {
+                    count[i]++;
+                    ch[j] = '0';
+                }
+            }
+        }
+        for (int i = 0; i < count.length; i++) {
+            if (ch[i] != ' ' && ch[i] != '0')
+                
+                System.out.println("The frequency of " + st.charAt(i) + " is " + count[i]);
+        }
+    }
+}
