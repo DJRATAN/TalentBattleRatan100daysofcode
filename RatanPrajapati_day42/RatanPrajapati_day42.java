6c11d90e5989947dc5f3a168d0cf9403a5077ac6
Ratan Prajapati <ratanprajapti1242@gmail.com>
2023-03-02 23:52:31 +0530

    28-02-2023 day 42 challenge

--- RatanPrajapati_day42/RatanPrajapati_day42.java
+++ RatanPrajapati_day42/RatanPrajapati_day42.java
@@ -12,7 +12,7 @@ public class RatanPrajapati_day42 {
         }
         return true;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the size of first array :");
