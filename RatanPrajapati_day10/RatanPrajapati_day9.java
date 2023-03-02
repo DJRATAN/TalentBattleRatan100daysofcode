ce0dbc5618b497ff5e9809caf8f5c1407a595315
Ratan Prajapati <ratanprajapti1242@gmail.com>
2023-03-02 23:57:56 +0530

    27-01-2023 day 10 challenge

--- RatanPrajapati_day10/RatanPrajapati_day9.java
+++ RatanPrajapati_day10/RatanPrajapati_day9.java
@@ -11,6 +11,6 @@ public class RatanPrajapati_day9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        System.out.println(factorial(num));
+        System.out.println(factorial(num)); 
     }
 }
