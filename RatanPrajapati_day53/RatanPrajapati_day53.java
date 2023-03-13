41292d4277cfe8ac5e05703e12a32027aae57a83
Ratan Prajapati <ratanprajapti1242@gmail.com>
2023-03-13 21:30:26 +0530

    11-03-2023 day 53 challenge

---
+++ RatanPrajapati_day53/RatanPrajapati_day53.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RatanPrajapati_day53 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int ans = arr[0];
+        for (int i = 0; i < size; i++) {
+            int prod = arr[i];
+            for (int j = i + 1; j < size; j++) {
+                ans = Math.max(ans, prod);
+                prod = prod * arr[j];
+            }
+            ans = Math.max(ans, prod);
+        }
+        System.out.println(ans);
+    }
+}
